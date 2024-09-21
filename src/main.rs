512f8a2ca6fdbfee5abef79f81066e9bb445cb9e
lexxycool <maxirexy16@yahoo.com>
2024-09-21 20:51:40 +0300

    changes for text editor in rust

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::io::{self, Read};
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+
+fn main() {
+    enable_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+        println!("{}", c);
+    }
+}
